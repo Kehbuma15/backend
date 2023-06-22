d552cbf2c416b77a4724e49076d9877a1968bc17
Kehbuma15 <kehbuma185@gmail.com>
2023-06-22 16:47:12 +0100

    first commit

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+    DB:'mongodb+srv://kehbuma185:MPl90BMIUmdbArEY@reactbackendportfolio.er28ip8.mongodb.net/?retryWrites=true&w=majority'
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const {MONGO_DB_CONFIG} = require("./config/app.config");
+const cors = require('cors');
+
+//CONNECTION TO MONGODB
+mongoose.connect(MONGO_DB_CONFIG.DB,{
+    useNewUrlParser: true,
+    UseUnifiedTopology: true
+}).then(() =>{
+    console.log('Connection to MongoDB Succesful');
+}).catch((error) => {
+    console.log('Error connecting to MongoDB:',error);
+});
+
+//Enable Cors
+app.use(cors());
+
+//Middleware to pass JSON data
+app.use(express.json());
+
+//Define a route a fetch the data
+app.use("/api", require("./router/app.route"));
+
+//Start the server
+app.listen(8000, () =>{
+    console.log("Server started on port 8000 ");
+})
+   
+
+
